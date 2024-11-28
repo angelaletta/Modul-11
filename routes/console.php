2adab3f6a03e4649fe6bfd63c77573c092a81527
angelaletta <angelaletta17@gmail.com>
2024-11-28 09:38:57 +0700

    Add files via upload

---
+++ routes/console.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Foundation\Inspiring;
+use Illuminate\Support\Facades\Artisan;
+
+/*
+|--------------------------------------------------------------------------
+| Console Routes
+|--------------------------------------------------------------------------
+|
+| This file is where you may define all of your Closure based console
+| commands. Each Closure is bound to a command instance allowing a
+| simple approach to interacting with each command's IO methods.
+|
+*/
+
+Artisan::command('inspire', function () {
+    $this->comment(Inspiring::quote());
+})->purpose('Display an inspiring quote');
+
+Route::prefix('admin')->name('admin.')->group(function() {
+    Route::get('/dashboard', function() {
+        return "This is admin dashboard";
+    })->name('dashboard');
+    Route::get('/users', function() {
+        return "This is user data on admin page";
+    })->name('users');
+    Route::get('/items', function() {
+        return "This is item data on admin page";
+    })->name('items');
+});
